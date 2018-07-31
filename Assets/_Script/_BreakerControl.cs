7abd9bb130b66cc50f7bf07e1ded6d7b1b6536d1
MF Habibie <mfhabibie1906@gmail.com>
2018-07-31 15:23:12 +0700

    particle effect
    add particle effect chainsaw and glass breaker

---
+++ Assets/_Script/_BreakerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _BreakerControl : MonoBehaviour {
+
+    public ParticleSystem glass;
+    public AudioClip breakerHit;
+
+    // Use this for initialization
+    void Start () {
+        glass.Stop(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Glass")
+        {
+            Debug.Log("Hit");
+            glass.Play(true);
+
+            gameObject.GetComponent<AudioSource>().clip = breakerHit;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/_Script/_CutterControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _CutterControl : MonoBehaviour {
+
+    public ParticleSystem spark;
+    public AudioClip sawIdle, sawHit;
+    public GameObject scratch;
+    public Transform scratchLocate;
+    Vector3 fwd;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<AudioSource>().clip = sawIdle;
+        gameObject.GetComponent<AudioSource>().Play();
+
+        spark.Stop(true);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        fwd = transform.TransformDirection(Vector3.forward);
+        
+        Debug.DrawRay(transform.position, fwd * 10, Color.green);
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Cube")
+        {
+            Debug.Log("Hit");
+            spark.Play(true);
+
+            gameObject.GetComponent<AudioSource>().clip = sawHit;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube")
+        {
+            Ray ray = Camera.main.ScreenPointToRay(fwd);
+            RaycastHit hit = new RaycastHit();
+            GameObject dummy = Instantiate(scratch, scratchLocate.position, scratchLocate.rotation);
+            dummy.transform.position = new Vector3(scratchLocate.position.x, scratchLocate.position.y, 0.55f * collision.gameObject.transform.position.z);
+            //dummy.transform.rotation = Quaternion.Euler();
+            //Debug.Log(hit.distance);
+            
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube")
+        {
+            gameObject.GetComponent<AudioSource>().clip = sawIdle;
+            gameObject.GetComponent<AudioSource>().Play();
+            Debug.Log("Out");
+            spark.Stop(true);
+        }
+    }
+}
